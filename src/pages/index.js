d938eea0bde7daffd7b6dc26912284c15c11fc06
qq8721443 <qq8721443@naver.com>
2021-10-16 23:51:08 +0900

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorials.</p>
+      <StaticImage
+        alt="Canyon in my local"
+        src="../images/canyon.jpg"
+      />
     </Layout>
   )
 }

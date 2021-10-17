04da4301185e298b9e17c48f7ebee9f40d496c8e
qq8721443 <qq8721443@naver.com>
2021-10-17 12:52:30 +0900

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,22 +5,28 @@ import Layout from '../components/layout';
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map((item, index) => (
-                        <li key={index}>{item.name}</li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Date: {node.frontmatter.date}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
 {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "YYYY-MM-DD")
+        }
+        id
+        body
       }
     }
   }
